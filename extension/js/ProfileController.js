41d1f74769b7f96db8e86fb812b63d0dde39e4dd
Jon Guerrera <jrrera@gmail.com>
2013-10-21 18:08:02 -0700

    Improves RegEx text processing and error handling

--- extension/js/ProfileController.js
+++ extension/js/ProfileController.js
@@ -4,10 +4,12 @@ dbotExtApp.controller('ProfileController',
 	function ProfileController($scope, $timeout, $filter, $log, ScraperData) {
 
 		function processLineBreaks(text) {
+			console.log('text prior to processing', text);
 		    var final = text.replace(/\s*<p[^>]+">\s*/gi,""); //Filters out all P tags
+		    final = final.replace(/\n*\s*<!--.*-->\s*\n*/gi, ""); //Removes commented out HTML from Angular
+		    final = final.replace(/\s*<br\s?\/?>\s*\n*<\/p>\n*\s*/gi, "\n\n");
 		    final = final.replace(/\s*<br\s?\/?>\s*/gi,"\n"); //Puts a line break for any <br> tag
 		    final = final.replace(/\s*<\/p>\s*/gi,"\n\n"); //Adds two lines breaks for any closing p tags
-		    final = final.replace(/<!--.*-->/gi, ""); //Removes commented out HTML from Angular
 		    return final;
 		}
 
@@ -28,28 +30,33 @@ dbotExtApp.controller('ProfileController',
 			//Returns an array of - in order - okcText, okcContext (for matched interests), the picture URL, and the jquery object
 			jqueryArr = ScraperData.turnIntoJquery(html);
 
-			//Returns an array of objects (the essays scraped from the profile)
-			okcContext = ScraperData.processContext(jqueryArr[3]);
+			//console.log(jqueryArr);
 
-			//Processes the profile text with find-and-replace
-			jqueryArr[0] = ScraperData.processProfileText(jqueryArr[0]); 
+			//Now, we test to see if the OKCText and username were found in the HTML. If not, we're not on a profile page. If yes, continue on
+			if (jqueryArr[0] && jqueryArr[1]) {
+				//Returns an array of objects (the essays scraped from the profile)
+				okcContext = ScraperData.processContext(jqueryArr[3]);
 
-			//Adds the okcContext as the fifth item in the jqueryArr array
-			jqueryArr.push(okcContext);
+				//Processes the profile text with find-and-replace
+				jqueryArr[0] = ScraperData.processProfileText(jqueryArr[0]); 
 
-			//Create the final profile object to send to the front-end
-			profileObj = {
-				okcText: jqueryArr[0],
-				okcUsername: jqueryArr[1],
-				okcPicture: jqueryArr[2],
-				okcContext: jqueryArr[4],
-				matches: ScraperData.findSimilarities(jqueryArr[0], $scope.keywords, jqueryArr[4]) //Generates the object that the Chrome extension front end looks for
+				//Adds the okcContext as the fifth item in the jqueryArr array
+				jqueryArr.push(okcContext);
+
+				//Create the final profile object to send to the front-end if the profile processed successfully
+				profileObj = {
+					okcText: jqueryArr[0],
+					okcUsername: jqueryArr[1],
+					okcPicture: jqueryArr[2],
+					okcContext: jqueryArr[4],
+					matches: ScraperData.findSimilarities(jqueryArr[0], $scope.keywords, jqueryArr[4]) //Generates the object that the Chrome extension front end looks for
+				}
+				
+				$scope.profiles.push(profileObj);
 			}
-			
-			$scope.profiles.push(profileObj);
+
 			$scope.loading = false;
 
-			console.log('$scope.profiles is defined as:', $scope.profiles);
 		}, function(e){
 			console.log(e);
 			$scope.loading = false;
@@ -83,7 +90,9 @@ dbotExtApp.controller('ProfileController',
 			var message, user, databaseData;
 			
 			message = processLineBreaks($('.finalmessage').html());
-			console.log('processLinebreaks message after processing is', message);
+
+			//$scope.testmessage = message; //for testing
+			//console.log('processLinebreaks message after processing is', message);
 
 			var portObj = {
 			  message: message
--- extension/js/filters.js
+++ extension/js/filters.js
@@ -1,27 +1,5 @@
 'use strict';
 
-// dbotExtApp.filter('filterMessaged', function() {
-// 	return function (profiles, value) {
-
-// 		var matchedArr = [];
-
-// 		// console.log('profiles', profiles);
-// 		// console.log('value', value);
-		
-// 		//value is boolean based on whether "checked message" box is checked
-// 		for (var i = 0; i < profiles.length; i++) {
-// 			if (profiles[i].messaged && !value) {
-// 				//console.log('do nothing');
-// 			} else {
-// 				matchedArr.push(profiles[i]);
-// 			}			
-// 		}
-
-// 		return matchedArr;
-
-// 	}
-// });
-
 dbotExtApp.filter('filterForDatabase', function() {
 	return function (profileObj) {
 
