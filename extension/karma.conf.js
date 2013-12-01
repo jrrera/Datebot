cc58e93281084b7bfa530490a397b1ceb22cc18b
Jon Guerrera <jrrera@gmail.com>
2013-11-30 18:23:54 -0800

    Adds Angular Sanitize module to karma runner and jasmine

--- extension/karma.conf.js
+++ extension/karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       "lib/angular.min.js",
+      "lib/angular-sanitize.min.js",
       "lib/angular-mocks.js",
       "js/app.js",
       "js/ScraperData.js",
