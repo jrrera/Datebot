711e0661c680154a3da2f453550a7ddaacab627c
Jon Guerrera <jrrera@gmail.com>
2013-12-28 13:23:32 -0800

    Improves message sending by leveraging OKC's action composer

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -6,16 +6,30 @@
 (function(){
     console.log('Datebot Extension script injected!');
 
-    function createScript(action) {
+    function createScript(action, composer) {
       //This function injects scripts into the OKC dom through the Content Script
       var scriptNode;
+
       if (action === "openWindow") {
         scriptNode = document.createElement('script');
-        scriptNode.textContent = "Profile.focusMessageCompose();";
+
+        // Open the composer window for main composer and action composer
+        scriptNode.textContent = "Profile.focusMessageCompose(); Profile.toggleActionCompose('on');";
+
+        // Inject script
         document.body.appendChild(scriptNode);
+
       } else if (action === "sendMessage") {
         scriptNode = document.createElement('script');
-        scriptNode.textContent = "Profile.sendMessage(); console.log('The deed has been done');";
+        
+        // Trigger the appropriate window on the page based on which message composer is open
+        if (composer === 'action_message') {
+          scriptNode.textContent = "Profile.actionSendMessage(); console.log('Message sent');";  
+        } else {
+          scriptNode.textContent = "Profile.sendMessage(); console.log('Message sent');";  
+        }
+        
+        // Inject the script into the page
         document.body.appendChild(scriptNode);
       }
     }
@@ -30,13 +44,26 @@
 
         //This indicates that we've clicked 'Send the Message' in the Chrome extension and received the final draft of the message, so we place the message in the appropriate divs, and execute a script to run the send message functionality
         if (msg.finalmessage) {
+          // Declare offset from top of page, and a container for which message composer to work with
+          var scrollTop = $(window).scrollTop(),
+              messageWindow; //Used to tell the script which message composer to trigger
+
           //Open the window composer on OKC
           createScript('openWindow');
            
           //2 seconds after opening the message window, we populate the container and send the message
           setTimeout(function(){
-            $('#message_text').val(msg.finalmessage.message);
-            createScript('sendMessage'); 
+            if (scrollTop > 486) {
+              // If we're too far down the page, we need to use the action_message container instead
+              $('#action_message').val(msg.finalmessage.message);
+              messageWindow = 'action_message';
+              console.log('sending via action_message window');  
+            } else {
+              $('#message_text').val(msg.finalmessage.message);  
+              messageWindow = 'message_text';
+              console.log('sending via standard message_text window');
+            }
+            createScript('sendMessage', messageWindow); 
           }, 2000);
 
           sendResponse({status:'success'});
@@ -52,7 +79,6 @@
             $('.unreadMessage, .readMessage, .repliedMessage, .filteredReadMessage').each(function(){
                 var text = $(this).text();
                 if (text.search(pattern) === -1) {
-                    //console.log ('Found a true reponse!');
                     namesArr.push(($(this).find('.subject').text()));
                 }
             });
